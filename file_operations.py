080364785bfa64eda03749bf18ae2c585e7b6a07
Elena Menyaylenko <menyaylenko@gmail.com>
2014-12-21 22:58:31 -0800

    working on data class

--- file_operations.py
+++ file_operations.py
@@ -1,23 +1,23 @@
 # Functions to do operations on hdf file
 
-def readFile(filename):
+def readFile(filename, data):
     import h5py
 
     f = h5py.File(filename, 'r')
 
-    time_data = f['/dru/capture/data']
-    labels = f['/dru/capture/labels']
-    sample_rate = f['/dru/capture/rdt/sample_rate']
-    pga_gain_code = f['/dru/capture/rdt/pga_gain_code']
+    data._time_data = f['/dru/capture/data']
+    data._labels = f['/dru/capture/labels']
+    data._sample_rate = f['/dru/capture/rdt/sample_rate']
+    data._pga_gain_code = f['/dru/capture/rdt/pga_gain_code']
 
-    print ("%s\t" % ("time_data"))
-    # print (sample_rate[()])
+    # print ("%s\t" % ("time_data"))
+    print (data._sample_rate[()])
     f.close()
 
 
 class Data:
     # constructor
-    def __init__(self, time_data, labels, sample_rate, pga_gain_code):
+    def __init__(self, time_data=[], labels=[], sample_rate=0, pga_gain_code=0):
         self._time_data = time_data
         self._labels = labels
         self._sample_rate = sample_rate
@@ -32,8 +32,7 @@ def sample_freq(self):
 def print_sample_rate(self):
     print self._sample_rate
 
+
     # destructor
-
-
-def __del__(self):
-    print "Data object destroyed"
+def release(self):
+    print "Data object destroyed, memory released"
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+#!usr/bin/python
 
 import pdb
 import file_operations as fo
 
 FNAME = '3rdFireTestSet.hdf5'
 
-def main():
-    fo.readFile(FNAME)
-    data = fo.Data(time_data,labels,sample_rate, pga_gain_code)
-    print (data._sample_rate)
 
-pdb.set_trace()
-#pass
+def main():
+    data = fo.Data()
+    fo.readFile(FNAME, data)
+    #print (data._sample_rate)
+    print("HELLO!")
+    #data.release()
+
+# pdb.set_trace()
+# pass
 if __name__ == "__main__":
     main()
 
-#print (sample_rate[()])
+    # print (sample_rate[()])
